406a73ed9efac6aad2a1fbf23c09ecdc268d041f
Konstantin Khoroshilov <manly41142009@yandex.ru>
2020-07-19 00:29:44 +0300

    feat: добавить скрипт для вызова popup

---
+++ script.js
@@ -0,0 +1,32 @@
+let editButton = document.querySelector('.profile__edit-button');//добавить в переменную ссылку на кнопку "Редактировать"
+let popupCloseButton = document.querySelector('.popup__close-button');//добавить в переменную ссылку на  кнопку "Закрыть"
+let popupSaveButton = document.querySelector('.popup__save-button');//добавить в переменную ссылку на кнопку "Сохранить"
+let profileName = document.querySelector('.profile__name');////добавить в переменную ссылку на поле "имя" на странице
+let profileJob = document.querySelector('.profile__job');//добавить в переменную ссылку на поле "профессия" на странице
+let popup = document.querySelector('.popup');//добавить в переменную ссылку на  всплывающее окно
+let formElement = document.querySelector('.popup__container');//добавить в переменную ссылку на форму из всплывающего окна
+let popupName = document.querySelector('.popup__name');//добавить в переменную ссылку на поле "имя" в всплывающем окне
+let popupJob = document.querySelector('.popup__job');//добавить в переменную ссылку на поле "профессия" в всплывающем окне
+
+function displayPopup() {
+  popupName.value = profileName.textContent;//записать имя профиля со страницы в соответсвующее поле всплывающего окна
+  popupJob.value = profileJob.textContent;//записать профессию профиля со страницы в соответсвующее поле всплывающего окна
+  popup.classList.add('popup_opened');//добавить класс popup_opened, т.е. отобразить всплывающее окно
+}
+
+function closePopup() {
+  popup.classList.remove("popup_opened"); //удалить класс popup_opened, т.е. спрятать всплывающее окно
+}
+
+function formSubmitHandler (evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  profileName.textContent = popupName.value;//записать имя профиля из всплывающего окна в соответсвующее поле на странице
+  profileJob.textContent = popupJob.value;//записать профессию профиля из всплывающего окна в соответсвующее поле на странице
+  closePopup();
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler);
+editButton.addEventListener('click', displayPopup);//по клику на кнопку "Редактировать" выполнить displayPopup(), т.е. отобразить всплывающее окно
+popupCloseButton.addEventListener('click', closePopup);//по клику на кнопку "Закрыть" выполнить closePopup(), т.е.  спрятать всплывающее окно без сохранения
